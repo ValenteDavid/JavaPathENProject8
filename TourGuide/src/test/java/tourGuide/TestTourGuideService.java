abe8e1199f6a3330af3da0efe617cc56be7a2689
David Valente <dvalente77@gmail.com>
2021-09-26 19:45:42 +0200

    restaure to compare

--- TourGuide/src/test/java/tourGuide/TestTourGuideService.java
+++ TourGuide/src/test/java/tourGuide/TestTourGuideService.java
@@ -35,6 +35,28 @@ public class TestTourGuideService {
 	}
 	
 	@Test
+	public void addUser() {
+		GpsUtil gpsUtil = new GpsUtil();
+		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
+		InternalTestHelper.setInternalUserNumber(0);
+		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
+		
+		User user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
+		User user2 = new User(UUID.randomUUID(), "jon2", "000", "jon2@tourGuide.com");
+
+		tourGuideService.addUser(user);
+		tourGuideService.addUser(user2);
+		
+		User retrivedUser = tourGuideService.getUser(user.getUserName());
+		User retrivedUser2 = tourGuideService.getUser(user2.getUserName());
+
+		tourGuideService.tracker.stopTracking();
+		
+		assertEquals(user, retrivedUser);
+		assertEquals(user2, retrivedUser2);
+	}
+	
+	@Test
 	public void getAllUsers() {
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
